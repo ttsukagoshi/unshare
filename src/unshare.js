6767ed2b7b2c05511b08fb3fcedbf87955bf46a4
ttsukagoshi <taro.tsukagoshi@gmail.com>
2022-05-06 12:19:36 +0900

    Add the core "un"share process (#6) to remove users' access to the selected file/folder(s)

--- src/unshare.js
+++ src/unshare.js
@@ -16,8 +16,10 @@
    See the GitHub repository for more details: https://github.com/ttsukagoshi/unshare
 */
 
-const UP_KEY_FILE_USERS = 'fileUsers';
-const UP_KEY_IS_HOST_DRIVE = 'isHostDrive';
+const CACHE_EXPIRATION_IN_SEC = 3600; // TTL of user cache in seconds
+const ADDON_EXEC_TIME_LIMIT_IN_MILLISEC = 30 * 1000; // Execution time limit of Google Workspace Add-ons in milliseconds, as specified in https://developers.google.com/apps-script/add-ons/concepts/actions#callback_functions
+const ADDON_EXEC_TIME_LIMIT_WITH_BUFFER =
+  ADDON_EXEC_TIME_LIMIT_IN_MILLISEC * 0.9;
 
 //////////////////////////
 // Add-on Card Builders //
@@ -29,41 +31,37 @@ const UP_KEY_IS_HOST_DRIVE = 'isHostDrive';
  * @returns {Object} Google Workspace Add-on Card object.
  * @see https://developers.google.com/workspace/add-ons/concepts/event-objects
  */
-function buildHomepage(/*event*/) {
-  // console.log(JSON.stringify(event)); // debug
+function buildHomepage(event) {
+  console.log(JSON.stringify(event)); // debug
   // Build card
   let builder = CardService.newCardBuilder();
   // Message Section
   builder.addSection(
-    CardService.newCardSection()
-      .addWidget(
-        CardService.newTextParagraph().setText(
-          'Press the "Continue" button to stop sharing the files to your collaborators.\nNote that this process, which will delete all editors, commenters, and viewers from this file except for you, the owner, <b>IS IRREVERSIBLE</b>.'
-        )
+    CardService.newCardSection().addWidget(
+      CardService.newTextParagraph().setText(
+        'Press the "Continue" button to stop sharing the files with your collaborators.\n\nUnshare will delete all editors, commenters, and viewers from this file/folder except for you, the owner.\n\n<b>THIS PROCESS CANNOT BE UNDONE</b>.'
       )
-      .addWidget(
-        CardService.newButtonSet().addButton(
-          CardService.newTextButton()
-            .setText('Continue')
-            .setTextButtonStyle(CardService.TextButtonStyle.FILLED)
-            .setOnClickAction(
-              CardService.newAction().setFunctionName('buildConfirmationPage')
-            )
-        )
-      )
+    )
   );
   // Fixed Footer
   builder.setFixedFooter(
-    CardService.newFixedFooter().setPrimaryButton(
-      CardService.newTextButton()
-        .setText('HELP')
-        .setTextButtonStyle(CardService.TextButtonStyle.FILLED)
-        .setOpenLink(
-          CardService.newOpenLink().setUrl(
-            'https://www.scriptable-assets.page/add-ons/unshare/'
+    CardService.newFixedFooter()
+      .setPrimaryButton(
+        CardService.newTextButton()
+          .setText('CONTINUE')
+          .setOnClickAction(
+            CardService.newAction().setFunctionName('buildConfirmationPage')
           )
-        )
-    )
+      )
+      .setSecondaryButton(
+        CardService.newTextButton()
+          .setText('HELP')
+          .setOpenLink(
+            CardService.newOpenLink().setUrl(
+              'https://www.scriptable-assets.page/add-ons/unshare/'
+            )
+          )
+      )
   );
   return builder.build();
 }
@@ -75,8 +73,8 @@ function buildHomepage(/*event*/) {
  * @returns {Object} Google Workspace Add-on Card object.
  * @see https://developers.google.com/apps-script/add-ons/drive/building-drive-interfaces#drive_homepages
  */
-function buildDriveHomepage(/* event */) {
-  // console.log(JSON.stringify(event)); // debug
+function buildDriveHomepage(event) {
+  console.log(JSON.stringify(event)); // debug
   // Build card
   let builder = CardService.newCardBuilder();
   // Message Section
@@ -109,27 +107,10 @@ function buildDriveItemsSelected(event) {
  * @see https://developers.google.com/workspace/add-ons/concepts/event-objects
  */
 function buildConfirmationPage(event) {
-  // console.log(JSON.stringify(event)); // debug
-  const isHostDrive = event.commonEventObject.hostApp === 'DRIVE';
-  let fileIds = [];
+  console.log(JSON.stringify(event)); // debug
+  let isHostDrive = event.commonEventObject.hostApp === 'DRIVE';
   try {
-    switch (event.commonEventObject.hostApp) {
-      case 'DRIVE':
-        fileIds = event.drive.selectedItems.map((item) => item.id);
-        break;
-      case 'SHEETS':
-        fileIds.push = SpreadsheetApp.getActiveSpreadsheet().getId();
-        break;
-      case 'DOCS':
-        fileIds.push = DocumentApp.getActiveDocument().getId();
-        break;
-      case 'SLIDES':
-        fileIds.push = SlidesApp.getActivePresentation().getId();
-        break;
-      default:
-        throw new Error('Unshare is not available in this platform.');
-    }
-    let fileUsers = getFileUsers(fileIds);
+    let fileUsers = getFileUsers(event);
     if (fileUsers.isOwner.length > 0) {
       const targetFilesSummary = fileUsers.isOwner.reduce((text, file) => {
         let editorList = file.editors
@@ -142,9 +123,9 @@ function buildConfirmationPage(event) {
         return text;
       }, '');
       const ignoredFilesSummary =
-        fileUsers.notOwner.length === 0
+        fileUsers.isNotOwner.length === 0
           ? ''
-          : fileUsers.notOwner.reduce((text, file) => {
+          : fileUsers.isNotOwner.reduce((text, file) => {
               text += `\n * ${file.fileName}`;
               return text;
             }, 'Note that the files below will be ignored since you are not the owner:');
@@ -152,7 +133,7 @@ function buildConfirmationPage(event) {
       builder.addSection(
         CardService.newCardSection().addWidget(
           CardService.newTextParagraph().setText(
-            `Are you sure you want to proceed with Unshare? You will be permanently removing all editors and viewers (including commenters) from these file(s):${targetFilesSummary}\n<b>THIS ACTION CANNOT BE UNDONE</b>.\n\n${ignoredFilesSummary}`
+            `Are you sure you want to proceed with Unshare? You will be permanently removing all editors and viewers (including commenters) from these file/folder(s):${targetFilesSummary}\n<b>THIS ACTION CANNOT BE UNDONE</b>.\n\n${ignoredFilesSummary}`
           )
         )
       );
@@ -176,30 +157,58 @@ function buildConfirmationPage(event) {
               )
           )
       );
-      PropertiesService.getUserProperties().setProperty(
-        UP_KEY_IS_HOST_DRIVE,
-        isHostDrive
-      );
       return builder.build();
     } else {
-      let notOwnerFileNameList = fileUsers.notOwner
-        .map((notOwnerFile) => ` - ${notOwnerFile.fileName}`)
+      let isNotOwnerFileNameList = fileUsers.isNotOwner
+        .map((isNotOwnerFile) => ` - ${isNotOwnerFile.fileName}`)
         .join('\n');
       throw new Error(
-        `[Error] You must be the owner of the file(s) to execute Unshare:\n${notOwnerFileNameList}`
+        `[ERROR] You must be the owner of the file/folder(s) to execute Unshare:\n${isNotOwnerFileNameList}`
       );
     }
   } catch (error) {
     return createMessageCard(
-      error.stack,
+      filteredErrorMessage(error),
       event.commonEventObject.userLocale,
-      isHostDrive
+      event.commonEventObject.hostApp === 'DRIVE'
     );
   }
 }
 
 function unshare(event) {
   console.log(JSON.stringify(event)); // debug
+  const start = new Date();
+  try {
+    let fileUsers = getFileUsers(event, true);
+    fileUsers.isOwner.forEach((fileSummary, i, ownerArr) => {
+      let file = DriveApp.getFileById(fileSummary.fileId);
+      // Set the access status to PRIVATE
+      file.setSharing(DriveApp.Access.PRIVATE, DriveApp.Permission.VIEW);
+      // Remove Editors
+      fileSummary.editors.forEach((editor) => {
+        file.removeEditor(editor);
+        // Check the execution time and quit if the runtime is expected to exceeed the time limit
+        checkExecTime(start, i, ownerArr, false);
+      });
+      // Remove Viewers and Commentors
+      fileSummary.viewers.forEach((viewer) => {
+        file.removeViewer(viewer);
+        checkExecTime(start, i, ownerArr, false);
+      });
+      checkExecTime(start, i, ownerArr, true);
+    });
+    return createMessageCard(
+      'All file(s)/folder(s) have been "un"shared.',
+      event.commonEventObject.userLocale,
+      event.commonEventObject.hostApp === 'DRIVE'
+    );
+  } catch (error) {
+    return createMessageCard(
+      filteredErrorMessage(error),
+      event.commonEventObject.userLocale,
+      event.commonEventObject.hostApp === 'DRIVE'
+    );
+  }
 }
 
 /**
@@ -232,64 +241,126 @@ function createMessageCard(message, userLocale, isHostDrive = false) {
 }
 
 /**
- * Get the list of the files' respective owner, editors, and viewers (including commenters).
- * @param {Array} fileIds Array of Google Drive file IDs
- * @param {Boolean} returnUserObj [Optional] Returns user objects when true.
- * Defaults to false to return primary email addresses of the users.
+ * Get the list of the file/folders' respective owner, editors, and viewers (including commenters)
+ * based on the incoming Google Workspace Add-on event.
+ * Refers to info saved in user cache whenever available.
+ * @param {Object} event Google Workspace Add-on Event object.
+ * @param {Boolean} useCache Will not refer to user cache when false, the default value.
  * @returns {Object} Summarized JavaScript objects of the input files with their respective owner, editors, and viewers.
  */
-function getFileUsers(fileIds) {
+function getFileUsers(event, useCache = false) {
+  const cache = CacheService.getUserCache();
+  // Get the array of target file/folder IDs from the event object
+  let fileIds = [];
+  switch (event.commonEventObject.hostApp) {
+    case 'DRIVE':
+      fileIds = event.drive.selectedItems.map((item) => item.id);
+      break;
+    case 'SHEETS':
+      fileIds.push(SpreadsheetApp.getActiveSpreadsheet().getId());
+      break;
+    case 'DOCS':
+      fileIds.push(DocumentApp.getActiveDocument().getId());
+      break;
+    case 'SLIDES':
+      fileIds.push(SlidesApp.getActivePresentation().getId());
+      break;
+    default:
+      throw new Error('[ERROR] Unshare is not available on this platform.');
+  }
+  // Get the list of users who have access to the target files/folders
   let fileUsers = fileIds.reduce(
     (obj, fileId) => {
-      const file = DriveApp.getFileById(fileId);
-      const fileName = file.getName();
-      const owner = file.getOwner();
-      const ownerEmail = owner.getEmail();
-      const editors = file.getEditors().reduce(
-        (editorsObj, editorUser) => {
+      const cachedFileUserStr = cache.get(fileId);
+      let fileUser;
+      if (!cachedFileUserStr || !useCache) {
+        // If the user info is not available as user cache
+        const file = DriveApp.getFileById(fileId);
+        const ownerEmail = file.getOwner().getEmail();
+        // Get the list of editors
+        const editors = file.getEditors().reduce((arr, editorUser) => {
           let editorEmail = editorUser.getEmail();
           if (editorEmail !== ownerEmail) {
-            editorsObj.editorUsers.push(editorUser);
-            editorsObj.editorEmails.push(editorEmail);
+            arr.push(editorEmail);
           }
-          return editorsObj;
-        },
-        { editorUsers: [], editorEmails: [] }
-      );
-      const viewers = file.getViewers().reduce(
-        (viewersObj, viewerUser) => {
+          return arr;
+        }, []);
+        // Get the list of viewers and commentors
+        const viewers = file.getViewers().reduce((arr, viewerUser) => {
           let viewerEmail = viewerUser.getEmail();
-          if (
-            viewerEmail !== ownerEmail &&
-            !editors.editorEmails.includes(viewerEmail)
-          ) {
-            viewersObj.viewerUsers.push(viewerUser);
-            viewersObj.viewerEmails.push(viewerEmail);
+          if (viewerEmail !== ownerEmail && !editors.includes(viewerEmail)) {
+            arr.push(viewerEmail);
           }
-          return viewersObj;
-        },
-        { viewerUsers: [], viewerEmails: [] }
-      );
-      let fileUser = {
-        fileName: fileName,
-        owner: ownerEmail,
-        editors: editors.editorEmails,
-        viewers: viewers.viewerEmails,
-      };
+          return arr;
+        }, []);
+        fileUser = {
+          fileId: fileId,
+          fileName: file.getName(),
+          fileType: file.getMimeType(),
+          owner: ownerEmail,
+          editors: editors,
+          viewers: viewers,
+        };
+        // Save file info as user cache
+        cache.put(fileId, JSON.stringify(fileUser), CACHE_EXPIRATION_IN_SEC);
+      } else {
+        fileUser = JSON.parse(cachedFileUserStr);
+      }
       if (fileUser.owner === Session.getActiveUser().getEmail()) {
         obj.isOwner.push(fileUser);
       } else {
-        obj.notOwner.push(fileUser);
+        obj.isNotOwner.push(fileUser);
       }
       return obj;
     },
-    { isOwner: [], notOwner: [] }
+    { isOwner: [], isNotOwner: [] }
   );
-  PropertiesService.getUserProperties().setProperty(
-    UP_KEY_FILE_USERS,
-    JSON.stringify(fileUsers)
-  );
+  console.log(JSON.stringify(fileUsers)); // debug
   return fileUsers;
+}
+
+/**
+ * Checks the current add-on execution time and returns a time-out error when applicable
+ * @param {Date} startTime Date object indicating the start time of the add-on execution
+ * @param {Number} currentIndex Current index
+ * @param {Array} originalFileArray The original array of target files
+ * @param {Boolean} isFileEnd Indicator to show if this check function is executed at the end of a single file process. Defaults to false.
+ */
+function checkExecTime(
+  startTime,
+  currentIndex,
+  originalFileArray,
+  isFileEnd = false
+) {
+  if (
+    new Date().getTime() - startTime.getTime() >=
+    ADDON_EXEC_TIME_LIMIT_WITH_BUFFER
+  ) {
+    let slicePos = isFileEnd ? currentIndex + 1 : currentIndex;
+    throw new Error(
+      `[Exceeded Time Limit]\nThe execution time of Unshare is limited to ${
+        ADDON_EXEC_TIME_LIMIT_IN_MILLISEC / 1000
+      } seconds by Google. It could not finish "un"sharing the following file(s)/folder(s) due to this limit:\n${originalFileArray
+        .slice(slicePos)
+        .map((file) => file.fileName)
+        .join('\n')}`
+    );
+  } else {
+    return null;
+  }
+}
+
+/**
+ * Determines if the error is predefined and returns a simple error message in such cases.
+ * If the error is not defined, this function will return a full error message in `error.stack`.
+ * @param {Object} error Exceptions thrown in the script
+ * @returns {String}
+ */
+function filteredErrorMessage(error) {
+  return error.message.startsWith('[ERROR] ') ||
+    error.message.startsWith('[Exceeded Time Limit]\n')
+    ? error.message
+    : error.stack;
 }
 
 if (typeof module === 'object') {
