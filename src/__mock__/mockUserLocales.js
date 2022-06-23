d31e4730b0f62b91b3814c13b60b470bfcc26389
ttsukagoshi <taro.tsukagoshi@gmail.com>
2022-06-23 12:01:24 +0900

    Add "unknown" to list of mock user locales

--- src/__mock__/mockUserLocales.js
+++ src/__mock__/mockUserLocales.js
@@ -1,6 +1,10 @@
 const { MESSAGES } = require('../unshare');
 let mockMessages = MESSAGES;
 mockMessages['xx'] = {};
-const MOCK_USER_LOCALES = Object.keys(mockMessages).concat(['en-GB', 'zz']);
+const MOCK_USER_LOCALES = Object.keys(mockMessages).concat([
+  'en-GB',
+  'zz',
+  'unknown',
+]);
 
 module.exports = { MOCK_USER_LOCALES };
